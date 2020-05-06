f90b95b52bfd6ba2577ef422d912db1cba5ed104
Kapil Gautam <kapil.gautam@utdallas.edu>
2020-05-06 01:18:38 -0500

    Add heroku postbuild script

--- backend/backend.js
+++ backend/backend.js
@@ -1,31 +1,32 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 // var cors = require("cors");
 
 var app = express();
 const port = process.env.PORT || 5000;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 //DB configuration
-const db = require('./config/keys').mongoURI;
-mongoose.connect(db)
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db)
   .then(() => console.log("MongoDB Mongoose connected"))
   .catch((err) => console.log(err));
 
-var indexRouter = require('./routes/index');
-var itemRouter = require('./routes/api/items');
+var indexRouter = require("./routes/index");
+var itemRouter = require("./routes/api/items");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 // app.use(cors());
-app.use('/', indexRouter);
-app.use('/api/items', itemRouter);
+app.use("/", indexRouter);
+app.use("/api/items", itemRouter);
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
@@ -33,27 +34,26 @@ app.use('/api/items', itemRouter);
 // });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-// // if (process.env.NODE_ENV === 'production') {
-//   // Serve any static files
-//   app.use(express.static(path.join(__dirname, 'build')));
-    
-//   // Handle React routing, return all requests to React app
-//   app.get('*', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//   });
-// // }
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 
-// app.listen(process.env.PORT || PORT);
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = app;
